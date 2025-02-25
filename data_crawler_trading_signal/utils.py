40fc517b98bd1120df198f4593891154ea58e510
sam99 <samuele.piovan99@gmail.com>
2025-02-25 23:38:58 +0100

    :sparkles: feat(crawler): add utils module with magic number generator

---
+++ data_crawler_trading_signal/utils.py
@@ -0,0 +1,10 @@
+import random
+
+# -------------------------- Funzioni per il magic number --------------------------
+
+def generate_magic():
+    """
+    Genera un magic number casuale (stringa di 5 cifre).
+    Puoi sostituire questa logica con qualcosa di più sofisticato se necessario.
+    """
+    return str(random.randint(10000, 99999))
